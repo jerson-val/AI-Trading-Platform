b00da7cd8b564de525b64dcacfabba404ab01259
Jerson valencia <jerson.valencia@flyr.com>
2026-06-23 13:23:05 -0500

    fix loader

--- src/components/ui/fullscreen-loader.tsx
+++ src/components/ui/fullscreen-loader.tsx
@@ -1,16 +1,21 @@
 'use client'
 
+import { useAuthStore } from '@/src/store/auth.store'
 import { useLoaderStore } from '@/src/store/loader.store'
 
 export default function FullscreenLoader() {
-  const isLoading =
-    useLoaderStore(
-      (state) => state.count > 0
-    )
 
-  if (!isLoading) {
-    return null
-  }
+  const isAuthLoading = useAuthStore(
+    (s) => s.isAuthLoading
+  )
+
+  const count = useLoaderStore(
+    (s) => s.count
+  )
+
+  const isLoading = isAuthLoading || count > 0
+
+  if (!isLoading) return null
 
   return (
     <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/50 backdrop-blur-sm">
